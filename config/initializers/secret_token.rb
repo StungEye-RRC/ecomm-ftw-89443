308da92b8fb73d0bb9a55ed3d3cb6a8a343a6423
Wally Glutton <stungeye@gmail.com>
2013-11-13 10:26:22 -0600

    Initial commit of a default Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommFtw::Application.config.secret_token = 'e75d7b6fa6bd71abc5e729d8155bd98de0f7165b91ca19573771ed3ed6c1fdfcc8c4bb912b00ff686391a209c2064d495a5dfd35ef396dd42080b55148cc9d80'
